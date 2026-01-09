8b8fb60a1bbbc50dbbab7efe655c188c084e2858
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2026-01-09 18:10:05 +0530

    feat(auth):created user entity and repository

--- server/auth-service/src/main/java/com/example/auth_service/entity/User.java
+++ server/auth-service/src/main/java/com/example/auth_service/entity/User.java
@@ -1,4 +1,28 @@
 package com.example.auth_service.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
--- server/auth-service/src/main/java/com/example/auth_service/repository/UserRepository.java
+++ server/auth-service/src/main/java/com/example/auth_service/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.example.auth_service.repository;
 
-public class UserRepository {
+import com.example.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    // login purpose
+    Optional<User> findByEmail(String email);
+
+    // register purpose
+    boolean existsByEmail(String email);
 }
